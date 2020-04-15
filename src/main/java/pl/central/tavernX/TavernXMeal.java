89013d6f0bd44a116c905712d815c61483fbcbb6
SebastianW1991 <sebastianw1991@gmail.com>
2020-04-15 14:05:10 +0200

    Update TavernXMeal.java

--- src/main/java/pl/central/tavernX/TavernXMeal.java
+++ src/main/java/pl/central/tavernX/TavernXMeal.java
@@ -1,15 +1,31 @@
 package pl.central.tavernX;
 
+import pl.central.ActionStateContext;
+import pl.central.MarketX;
 import pl.central.PlayerActionState;
 
+import static pl.central.Main.getUserInput;
+import static pl.central.Main.playerWealth;
+
 public class TavernXMeal extends TavernX implements PlayerActionState {
-    private double tavernXMealPrice;
+    public double TavernXMealPrice = 2.0;
+    @Override
+    public void action(ActionStateContext ctx) {
 
-    public double getTavernXMealPrice() {
-        return 10;
+        System.out.println("You are about to buy Beer for 0.5 guldens, " +
+                "\n [a] Proceed" +
+                "\n [b] Cancel and leave Tavern ");
+
+
+        String playerChoice = getUserInput();
+        ActionStateContext stateContext = new ActionStateContext();
+
+        if (playerChoice.equals("a")) {
+            playerWealth = playerWealth - TavernXMealPrice;
+            stateContext.setState(new MarketX());
+        }
+        stateContext.setState(new MarketX());
     }
 
-    public void setTavernXMealPrice(double tavernXMealPrice) {
-        this.tavernXMealPrice = tavernXMealPrice;
-    }
 }
+
