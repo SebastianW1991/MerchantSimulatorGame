fa5ffcbccac8b3923ed6d6bc28e0a3b881e5f4d1
SebastianW1991 <sebastianw1991@gmail.com>
2020-04-22 18:19:01 +0200

    Armor shop products
    Added some buying options to armor shop

---
+++ src/main/java/pl/central/marketX/armorshopX/MarketXKiteShield.java
@@ -0,0 +1,48 @@
+package pl.central.marketX.armorshopX;
+
+import pl.central.ActionStateContext;
+import pl.central.PlayerActionState;
+import pl.central.marketX.MarketX;
+
+import static pl.central.Main.getUserInput;
+import static pl.central.Main.playerWealth;
+
+public class MarketXKiteShield implements PlayerActionState {
+    public double KiteShieldPriceX = 150.0;
+    @Override
+    public void action(ActionStateContext ctx) {
+        System.out.println("You are about to buy  kite shields for 150 guldens " +
+                "\n [a] Proceed and leave Shop" +
+                "\n [b] Cancel and ask for something else"+
+                "\n [b] Cancel and leave Shop ");
+
+
+        String playerChoice = getUserInput();
+        ActionStateContext stateContext = new ActionStateContext();
+
+        if (playerChoice.equals("a")) {
+            if (playerWealth>=KiteShieldPriceX){
+                playerWealth = playerWealth - KiteShieldPriceX;
+                stateContext.setState(new MarketX());
+                stateContext.action();}
+            else {
+                System.out.println(" You have not enough money, you may buy something cheaper though.");
+                stateContext.setState(new MarketXArmorShop());
+                stateContext.action();
+            }
+        }
+        else if(playerChoice.equals("b")){
+            stateContext.setState(new MarketXArmorShop());
+            stateContext.action();
+        }
+        else if (playerChoice.equals("c")){
+            stateContext.setState(new MarketX());
+            stateContext.action();
+        }
+        else {
+            System.out.println("There is no such option");
+            stateContext.setState(new MarketXArmorShop());
+            stateContext.action();
+        }
+    }
+}
